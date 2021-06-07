7f97da035ccdb1a6e7a4a96345af995d0b8e91c9
Disware <85359896+Disware@users.noreply.github.com>
2021-06-06 21:08:10 -0500

    Update CounterBlox.lua

--- scripts/CounterBlox.lua
+++ scripts/CounterBlox.lua
@@ -22,7 +22,7 @@ local getsenv = getsenv or false
 local listfiles = listfiles or listdir or syn_io_listdir or false
 local isfolder = isfolder or false
 
-if (getrawmetatable == false) then return game.Players.LocalPlayer:Kick("Exploit not supported! Missing: getrawmetatable.") end
+--if (getrawmetatable == false) then return game.Players.LocalPlayer:Kick("Exploit not supported! Missing: getrawmetatable.") end
 if (http_request == false) then return game.Players.LocalPlayer:Kick("Exploit not supported! Missing: request.") end
 if (mousemove == false) then return game.Players.LocalPlayer:Kick("Exploit not supported! Missing: mousemove.") end
 if (getsenv == false) then return game.Players.LocalPlayer:Kick("Exploit not supported! Missing: getsenv.") end
